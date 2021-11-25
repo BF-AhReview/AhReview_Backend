c8423884703536779746845954d5af60fd91ae9a
dohyeon5626 <kodohyeon71@gmail.com>
2021-11-25 23:41:56 +0900

    Add Get Review

---
+++ src/main/java/com/bulgogifriedrice/backend/domain/review/dto/ReviewGetDto.java
@@ -0,0 +1,50 @@
+package com.bulgogifriedrice.backend.domain.review.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+public class ReviewGetDto {
+
+    @Getter
+    @NoArgsConstructor
+    @ApiModel(value = "리뷰 가져오기 request", description = "리뷰 가져오기 request")
+    public static class Request {
+
+        @NotBlank
+        @ApiModelProperty(value = "가게 주소", example = "울릉도")
+        private String address;
+
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @ApiModel(value = "리뷰 가져오기 response", description = "리뷰 가져오기 response")
+    public static class Response {
+
+        @ApiModelProperty(value = "가게 별점", example = "5.0")
+        private double starScore;
+
+        @ApiModelProperty(value = "유저명", example = "익명일 경우 null")
+        private String username;
+
+        @ApiModelProperty(value = "리뷰 긍정 여부", example = "true")
+        private boolean positive;
+
+        @ApiModelProperty(value = "가게 리뷰", example = "넘 좋아요")
+        private String review;
+
+        @ApiModelProperty(value = "챗봇 반응", example = "넘 좋아요")
+        private String answer;
+
+        @ApiModelProperty(value = "가게 사진 url 리스트", example = "[\"asdfasdfas.com\",\"asdfasdfas.com\"]")
+        private List<String> urlList;
+
+    }
+
+}
--- src/main/java/com/bulgogifriedrice/backend/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/bulgogifriedrice/backend/domain/review/repository/ReviewRepository.java
@@ -3,5 +3,11 @@ package com.bulgogifriedrice.backend.domain.review.repository;
 import com.bulgogifriedrice.backend.domain.review.entity.Review;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends CrudRepository<Review, Long> {
+
+    List<Review> findByStoreAddressOrderByIdDesc(String store_address);
+    List<Review> findByStoreAddressAndPositive(String store_address, boolean positive);
+
 }
